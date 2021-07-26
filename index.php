291fa55bbdd03b652b8f29224105d3a54c2f9f89
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-07-26 17:09:22 -0400

    agregada pagina index del proyecto para validaciones

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php session_start();
+
+require 'admin/config.php';
+require 'functions.php';
+
+// comprobar session
+if (isset($_SESSION['usuario'])) {
+  // validar los datos por privilegio
+  $conexion = conexion($bd_config);
+  $usuario = iniciarSession('users', $conexion);
+
+  if ($usuario['user_type'] == '3') {
+    header('Location: '.RUTA.'adm/adm_page.php');
+    //hay que crear uno para cada tipo de usuario -- que serian prod, tecnicos y pruebas
+  } elseif ($usuario['user_type'] == '1') {
+    header('Location: ../admin/user_without_access.php');
+  } else {
+    header('Location: '.RUTA.'login/login.php');
+  }
+} else {
+  header('Location: '.RUTA.'login/login.php');
+}
+
+ ?>
