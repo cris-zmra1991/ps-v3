e6d59782b74c422a0c84eb116af953a5d29d7e8d
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-07-29 14:03:18 -0400

    Creado menu principal estatico y el iframe principal de la interfaz de usuario

---
+++ user/user.view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="../res/css/semantic.min.css">
+    <link rel="stylesheet" href="../res/css/main.css">
+
+  </head>
+  <body>
+    <div class="container_top_user">
+    <div class="ui inverted menu">
+      <div class="item">
+      <img src="../res/img/logo.png" height="25" >
+    </div>
+    <a class="active item">
+      Inicio
+    </a>
+    <a class="item">
+      Gestión de usuarios
+    </a>
+    <a class="item">
+      Inventarios
+    </a>
+    <a class="item">
+      Área técnica
+    </a>
+    <a class="item">
+      Producción
+    </a>
+    <a class="item">
+      Calidad
+    </a>
+  </div>
+</div>
+    <iframe class="iframe_user_content" src="first.php"></iframe>
+  </body>
+  </html>
