12049818d61fda520954057ab584db9f1d6e159b
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-07-30 13:47:06 -0400

    agregado modulo de gestion de usuarios

---
+++ usermg/usermg.view.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <link rel="stylesheet" href="../res/css/semantic.min.css">
+  <link rel="stylesheet" href="../res/css/main.css">
+  <body>
+
+  </body>
+</html>
