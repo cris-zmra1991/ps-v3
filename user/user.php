a9d1a9013ffe76df97243a6808410790a962bbb5
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-08-03 13:04:58 -0400

    agregada validacion

--- user/user.php
+++ user/user.php
@@ -1,3 +1,15 @@
 <?php
+require '../res/php/app_top.php';
+if (isset($_SESSION['usuario'])) {
+  $usuario = $_SESSION['usuario'];
+  if (!empty($usuario)) {
+    $data = $database->select("users","*",["user" => $usuario]);
+    }
+  else{
+    echo "No esta autenticado";
+  }
+
 require 'user.view.php';
+}
+
 ?>
