d2ae573425b83129fec70d57fe472de85468ac68
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-08-02 21:46:12 -0400

    caambios en la estructura del codigo y eliminacion de espacios y saltos

--- usermg/usermg.view.php
+++ usermg/usermg.view.php
@@ -8,55 +8,42 @@
   <link rel="stylesheet" href="../res/css/main.css">
 
   <body>
-
-  <div class="container_top_newuser">
-    <div class="div_subpage ui grid">
-    <div class="two wide column">
-      <div class="ui fluid vertical menu">
-  <div class="item">
-    <div class="header">Usuarios</div>
-    <div class="menu">
-      <a class="item" href="userlist.php" target="iframe_usermg_content">Listado de usuarios</a>
-      <a class="item" href="soliclist.php" target="iframe_usermg_content">Listado de solicitudes</a>
-    </div>
-  </div>
-  <div class="item">
-    <div class="header">Actualización</div>
-    <div class="menu">
-      <a class="item" href="usermod.php" target="iframe_usermg_content">Modificar usuarios</a>
-      <a class="item" href="userdel.php" target="iframe_usermg_content">Eliminar usuarios</a>
-    </div>
-  </div>
-  <div class="item">
-    <div class="header">Solicitudes</div>
-    <div class="menu">
-      <a class="item">Listar solicitudes</a>
-      <a class="item">Procesar solicitudes</a>
-    </div>
-  </div>
-  <div class="item">
-    <div class="header">Support</div>
-    <div class="menu">
-      <a class="item">E-mail Support</a>
-      <a class="item">Preguntas Frequentes</a>
-    </div>
-  </div>
-</div>
-    </div>
-    <div class="fourteen wide column">
-      <div class="div_subpage ui segment">
-
-      <iframe class="iframe_subpage" src="" name="iframe_usermg_content" id="iframe_usermg_content"></iframe>
+    <div class="container_top_newuser">
+      <div class="div_subpage ui grid">
+      <div class="two wide column">
+    <div class="ui fluid vertical menu">
+    <div class="item">
+      <div class="header">Usuarios</div>
+      <div class="menu">
+        <a class="item" href="userlist.php" target="iframe_usermg_content">Listado de usuarios</a>
       </div>
     </div>
-  </div>
-</div>
+    <div class="item">
+      <div class="header">Actualización</div>
+      <div class="menu">
+        <a class="item" href="usermod.php" target="iframe_usermg_content">Modificar usuarios</a>
+        <a class="item" href="userdel.php" target="iframe_usermg_content">Eliminar usuarios</a>
+      </div>
+    </div>
+    <div class="item">
+      <div class="header">Solicitudes</div>
+      <div class="menu">
+        <a class="item" href="soliclist.php" target="iframe_usermg_content">Listar solicitudes</a>
+        <a class="item">Procesar solicitudes</a>
+      </div>
+    </div>
+    </div>
+      </div>
+      <div class="fourteen wide column">
+        <div class="div_subpage ui segment">
 
-<script type="text/javascript" src="../res/js/jquery.min.js"></script>
-<script type="text/javascript" src="../res/js/semantic.min.js"></script>
+        <iframe class="iframe_subpage" src="" name="iframe_usermg_content" id="iframe_usermg_content"></iframe>
+        </div>
+      </div>
+    </div>
+    </div>
 
-<script type="text/javascript">
-$('.tabular.menu .item').tab();
-</script>
+  <script type="text/javascript" src="../res/js/jquery.min.js"></script>
+  <script type="text/javascript" src="../res/js/semantic.min.js"></script>
   </body>
 </html>
