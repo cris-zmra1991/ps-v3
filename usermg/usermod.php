d6b6d7c910274aa1b30b8f9ff5b90814bfc8644c
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-08-04 15:09:19 -0400

    agregada validacion de permisos

--- usermg/usermod.php
+++ usermg/usermod.php
@@ -1,5 +1,14 @@
 <?php
 require '../res/php/app_top.php';
-
-require 'usermod.view.php';
+$usuario = $_SESSION['usuario'];
+$usuarioid = $_SESSION['id_user'];
+if (!empty($usuario)) {
+  $data = $database->count("user_has_permissions",["AND" => ["id_user" => $usuarioid, "id_perm" => 3]]);
+  if (!empty($data)) {
+    require 'usermod.view.php';
+  }
+  else {
+  require 'e125.php';
+  }
+}
 ?>
