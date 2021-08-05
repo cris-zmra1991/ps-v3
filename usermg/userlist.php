e69ca5e4b4532c6cd31ec1245396771d7748acc8
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-08-04 21:36:19 -0400

    agregada validacion de permisos a listado de usuario

--- usermg/userlist.php
+++ usermg/userlist.php
@@ -1,5 +1,14 @@
 <?php
 require '../res/php/app_top.php';
-
-require 'userlist.view.php';
+$usuario = $_SESSION['usuario'];
+$usuarioid = $_SESSION['id_user'];
+if (!empty($usuario)) {
+  $data = $database->count("user_has_permissions",["AND" => ["id_user" => $usuarioid, "id_perm" => 4]]);
+  if (!empty($data)) {
+    require 'userlist.view.php';
+  }
+  else {
+  require 'e125.php';
+  }
+}
 ?>
