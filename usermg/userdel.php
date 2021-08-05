43dd84bad99df3db4e9d7e27e70dda8005ff5cfc
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-08-04 22:28:40 -0400

    descomentada validacion

--- usermg/userdel.php
+++ usermg/userdel.php
@@ -1,14 +1,14 @@
 <?php
 require '../res/php/app_top.php';
-//$usuario = $_SESSION['usuario'];
-//$usuarioid = $_SESSION['id_user'];
-//if (!empty($usuario)) {
-//  $data = $database->count("user_has_permissions",["AND" => ["id_user" => $usuarioid, "id_perm" => 2]]);
-//  if (!empty($data)) {
+$usuario = $_SESSION['usuario'];
+$usuarioid = $_SESSION['id_user'];
+if (!empty($usuario)) {
+  $data = $database->count("user_has_permissions",["AND" => ["id_user" => $usuarioid, "id_perm" => 2]]);
+  if (!empty($data)) {
     require 'userdel.view.php';
-//  }
-//  else {
-//  require 'e125.php';
-//  }
-//}
+  }
+  else {
+  require 'e125.php';
+  }
+}
 ?>
